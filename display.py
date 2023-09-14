88ab4706c9ca137e5c4d54a64074831bef44b8f3
bigshezhang <bigshezhang@gmail.com>
2023-09-15 03:46:45 +0800

    [bug] 白忙活的纯七色

--- display.py
+++ display.py
@@ -3,11 +3,12 @@ from flask_restful import Resource
 from datetime import datetime
 import sys
 from unit import Unit
-
+import c7_image
 class Display(Resource):
     def get(self):
         # 创建一个字节流（byte stream），这里示例为一个包含字母的二进制字符串
-        byte_stream = Unit.data
+        byte_stream = c7_image.Image7color
+        
         response = make_response(byte_stream)
         response.headers["Content-Type"] = "application/octet-stream"
         print({sys.getsizeof(byte_stream)})
