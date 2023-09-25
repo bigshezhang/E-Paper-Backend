c8a65b7103f4646fb15a083ed9c6a0cc052bd405
bigshezhang <bigshezhang@gmail.com>
2023-09-25 10:41:36 +0800

    [fix] mqtt - webtorpia

--- mqtt_server.py
+++ mqtt_server.py
@@ -4,23 +4,49 @@ import threading
 
 class MqttServer:
     
-    MQTT_BROKER_HOST = "e2782a8a.ala.cn-hangzhou.emqxsl.cn"
-    MQTT_BROKER_PORT = 8883
-    MQTT_TOPIC = "epaper/lazys_epaper_byte_stream"
+    # MQTT_BROKER_HOST = "e2782a8a.ala.cn-hangzhou.emqxsl.cn"
+    # MQTT_BROKER_PORT = 8883
+    # MQTT_TOPIC = "lazys_epaper_byte_stream"
+    
+    # def publish_file(self):
+    #     with open("./uploads/byte_stream.txt", "rb") as file:
+    #         print("MQTT 更新中")
+    #         data = file.read()
+    #         self.client.publish("lazys_epaper_byte_stream", payload=data, qos=2, retain=False)
+        
+    # def on_connect(client, userdata, flags, rc):
+    #     print("Connected to MQTT Broker")
+
+
+    # client = mqtt.Client("E-Paper-Server")
+    # client.tls_set(ca_certs='./emqxsl-ca.crt')
+    # client.username_pw_set('server', '12345678')
+    # client.on_connect = on_connect
+    # client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
+
+    # # MQTT连接回调函数
+
+    # # 启动MQTT客户端
+    # def mqtt_thread(self):
+    #     self.client.loop_forever()
+    
+    MQTT_BROKER_HOST = "213.202.219.45"
+    MQTT_BROKER_PORT = 1883
+    MQTT_TOPIC = "lazys_epaper_byte_stream"
     
     def publish_file(self):
         with open("./uploads/byte_stream.txt", "rb") as file:
             print("MQTT 更新中")
             data = file.read()
-            self.client.publish("epaper/lazys_epaper_byte_stream", payload=data, qos=2, retain=False)
+            self.client.publish("lazys_epaper_byte_stream", payload=data, qos=2, retain=False)
         
     def on_connect(client, userdata, flags, rc):
         print("Connected to MQTT Broker")
 
 
     client = mqtt.Client("E-Paper-Server")
-    client.tls_set(ca_certs='./emqxsl-ca.crt')
-    client.username_pw_set('server', '12345678')
+    # client.tls_set(ca_certs='./emqxsl-ca.crt')
+    # client.username_pw_set('server', '12345678')
     client.on_connect = on_connect
     client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
 
